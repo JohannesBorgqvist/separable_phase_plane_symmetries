766110fe9e26849dbbf3fa3c8b0ab42121cd176d
Johannes Borgqvist <borgqvist@maths.ox.ac.uk>
2022-07-12 11:20:05 +0100

    Save all the functions where we plot symmetries etc

--- Code/symmetry_toolbox.py
+++ Code/symmetry_toolbox.py
@@ -57,3 +57,49 @@ def transform_IC_LV(u0, v0, alpha, H):
     func = lambda v:  v+(alpha*u0)-log((u0**alpha)*v) - H
     v0 = fsolve(func, v0)[0]
     return v0
+# Function 6: dX_dt_linear
+# This is a help function defining the ODE system we want to solve.
+def dX_dt_linear(X, t=0,*parameters):
+    # Extract the parameters
+    a, b, c, d = parameters
+    # Return the dynamics of the linear system
+    return array([ a*X[0]+b*X[1],
+                   c*X[0]+d*X[1]])
+# Function 7: Gamma_r_ODE for the linear model
+def Gamma_r_ODE(X, epsilon=0,*parameters):
+    # Extract the parameters
+    a, b, c, d = parameters
+    # Return the dynamics of the linear system
+    return array([ X[0],X[1]])
+                   
+# Function 8: Gamma_theta_ODE for the linear model
+def Gamma_theta_ODE(X, epsilon=0,*parameters):
+    # Extract the parameters
+    a, b, c, d = parameters
+    # Return the dynamics of the linear system
+    return array([ -((c*X[0]**2+(d-a)*X[0]*X[1]-b*X[1]**2)/(a*X[0]**2+(b+c)*X[0]*X[1]+d*X[1]**2))*X[1],
+                   ((c*X[0]**2+(d-a)*X[0]*X[1]-b*X[1]**2)/(a*X[0]**2+(b+c)*X[0]*X[1]+d*X[1]**2))*X[0]])
+# Function 9: dX_dt_os for the oscillatory model
+def dX_dt_os(X, t=0,*parameters):
+    # Extract the parameters
+    omega = parameters[0]
+    # Define the radius
+    r = sqrt(X[0]**2+X[1]**2)    
+    # Return the dynamics of the linear system
+    return array([ X[0]*(1-r)-omega*X[1],
+                   X[1]*(1-r)+omega*X[0]])
+# Function 10: Gamma_r_os for the oscillatory model
+def Gamma_r_os(X, epsilon=0,*parameters):
+    # Extract the parameters
+    omega = parameters[0]
+    # Define the radius
+    r = sqrt(X[0]**2+X[1]**2)
+    # Return the dynamics of the biological oscillator
+    return array([(1-r)*X[0],(1-r)*X[1]])
+                   
+# Function 11: Gamma_theta_os for the oscillatory model
+def Gamma_theta_os(X, epsilon=0,*parameters):
+    # Extract the parameters
+    omega = parameters[0]
+    # Return the dynamics of the biological oscillator
+    return array([ -(X[1]/omega),(X[0]/omega)])
